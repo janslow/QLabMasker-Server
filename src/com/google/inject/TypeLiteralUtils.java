3062d976d3b420b4613f03711b548d64b86c2655
Jay Anslow <jay@anslow.me.uk>
2015-04-19 16:10:39 +0100

    Added Guice module for constructing painters.

---
+++ src/com/google/inject/TypeLiteralUtils.java
@@ -0,0 +1,27 @@
+package com.google.inject;
+
+import java.lang.reflect.Type;
+
+import com.google.inject.internal.MoreTypes.ParameterizedTypeImpl;
+import com.jayanslow.qlabMasker.painters.GLPainter;
+
+public final class TypeLiteralUtils {
+
+  public static <T> TypeLiteral<GLPainter<T>> painterOf(final Class<T> clazz) {
+    return typeLiteralOf(parameterize(GLPainter.class, clazz));
+  }
+
+  @Deprecated
+  private static Type parameterize(final Class<?> clazz, final Type parameter) {
+    return new ParameterizedTypeImpl(null, clazz, parameter);
+  }
+
+  @Deprecated
+  private static <T> TypeLiteral<T> typeLiteralOf(final Type type) {
+    return new TypeLiteral<T>(type) {
+    };
+  }
+
+  private TypeLiteralUtils() {
+  }
+}
---
+++ src/com/jayanslow/qlabMasker/painters/PaintersModule.java
@@ -0,0 +1,34 @@
+package com.jayanslow.qlabMasker.painters;
+
+import org.lwjgl.util.Color;
+import org.lwjgl.util.ReadableColor;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteralUtils;
+import com.google.inject.binder.LinkedBindingBuilder;
+import com.google.inject.name.Names;
+import com.jayanslow.qlabMasker.models.Polygon;
+import com.jayanslow.qlabMasker.models.Screen;
+
+public class PaintersModule extends AbstractModule {
+
+  public static final String NAME_MASK_COLOR = "MASK_COLOR";
+
+  public static final String NAME_UNMASK_COLOR = "UNMASK_COLOR";
+
+  private <T> LinkedBindingBuilder<GLPainter<T>> bindPainter(final Class<T> targetClass, final PainterMode targetMode) {
+    return bind(TypeLiteralUtils.painterOf(targetClass)).annotatedWith(TargetPainterModes.target(targetMode));
+  }
+
+  @Override
+  protected void configure() {
+    bindPainter(Screen.class, PainterMode.MASK).to(ScreenMaskGLPainter.class);
+    bindPainter(Polygon.class, PainterMode.MASK).to(PolygonMaskGLPainter.class);
+
+    bindPainter(Screen.class, PainterMode.INVERTED_MASK).to(ScreenInvertedMaskGLPainter.class);
+    bindPainter(Polygon.class, PainterMode.INVERTED_MASK).to(PolygonInvertedMaskGLPainter.class);
+
+    bind(ReadableColor.class).annotatedWith(Names.named(NAME_MASK_COLOR)).toInstance(new Color(0, 0, 0));
+    bind(ReadableColor.class).annotatedWith(Names.named(NAME_UNMASK_COLOR)).toInstance(new Color(255, 255, 255));
+  }
+}
